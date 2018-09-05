3e68dd4518805b726ab3fc5215bf1b2cf7e65e01
willshepp28 <willsheppard28@outlook.com>
2018-09-05 08:00:11 -0400

    changed button styles on /profile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
           directory: __dirname + '/db/migrations',
       },
       seeds: {
-          directory: __dirname + '/db/seeds/production',
+          directory: __dirname + '/db/seeds',
       },
   }
 }
